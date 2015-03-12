e57059682dbceec70d08e972346bb528f178802e
Christopher Biel <christopher.biel89@gmail.com>
2015-03-12 16:09:58 +0100

    add basics for header evaluation in launchAndWait

--- src/JenkinsApi/Item/Job.php
+++ src/JenkinsApi/Item/Job.php
@@ -145,10 +145,13 @@ class Job extends AbstractItem
         if (!$this->isCurrentlyBuilding()) {
             $lastNumber = $this->getLastBuild()->getNumber();
             $startTime = time();
-            $this->launch($parameters);
+            $response = $this->launch($parameters);
+            // TODO evaluate the response correctly, to get the queued item and later the build
+            if($response) {
+                list($header, $body) = explode("\r\n\r\n", $response, 2);
+            }
 
             $build = $this->getLastBuild();
-
             while ((time() < $startTime + $timeoutSeconds) && ($build->getNumber() == $lastNumber + 1 && !$build->isBuilding())) {
                 sleep($checkIntervalSeconds);
                 $build->refresh();
